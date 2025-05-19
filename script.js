b828dc1d6e4db86aba2346a33b92ad8576c2c3e8
temuriee <critstoggle@gmail.com>
2025-05-19 19:42:38 +0400

    bugfix

--- script.js
+++ script.js
@@ -6,20 +6,24 @@ let lastName = "Last Name : Jinjikhadze";
 let email = "Email : Example@gmail.com";
 let phoneNumber = 597321322;
 let age = 16;
-let hobby = "Hobby : WebDevelopment";
+let hobby = "WebDevelopment";
 let HeIsStudent = true;
 let question = "Is He Student? (Answer Bellow ⬇️)";
-let birthday = age + 1;
-let changehobby = "New Hobby : Football";
+// let birthday = age + 1;
+// let changehobby = "New Hobby : Football";
+console.log("My Age Is: " + age);
+console.log("My Hobby Is " + hobby);
+age = age + 1;
+hobby = "Football";
 
 // Putting It Out On Console
 console.log(firstName);
 console.log(lastName);
 console.log(email);
-console.log(hobby);
 console.log(question);
 console.log(HeIsStudent);
-console.log(age);
+console.log("My New Age Is " + age);
+console.log("My New Hobby Is " + hobby);
 // Update Age And Hobby
-console.log(birthday);
-console.log(changehobby);
+// console.log(birthday);
+// console.log(changehobby);
